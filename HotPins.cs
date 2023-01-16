c42ecd966d5883b7a6bb265088f81f1e1a8228b1
Andrew-Flame <andrewflame@mail.ru>
2023-01-16 13:36:47 +0300

    Initial commit

---
+++ HotPins.cs
@@ -0,0 +1,25 @@
+﻿using BepInEx;
+using HarmonyLib;
+using UnityEngine;
+
+namespace HotPins {
+    [BepInPlugin("Flame.HotPins", "HotPins", "1.0")]
+    [BepInProcess("valheim.exe")]
+    public class ExtendedMinimap : BaseUnityPlugin {
+        private readonly Harmony harmony = new Harmony("Flame.HotPins");
+
+        void Awake() {
+            harmony.PatchAll();  //Patching the harmony
+
+
+        }
+
+        /* Working with player markers */
+        [HarmonyPatch(typeof(Minimap), "UpdatePlayerMarker")]
+        class PlayerMarkerPatch {
+            private static void Postfix(ref RectTransform ___m_smallMarker, ref RectTransform ___m_smallShipMarker) {
+
+            }
+        }
+    }
+}
