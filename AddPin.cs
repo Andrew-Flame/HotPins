3f1736ed185b22622e6e511ab3677e84f9c14bbf
Andrew-Flame <andrewflame@mail.ru>
2023-01-16 17:18:04 +0300

    Add a command to the terminal that does nothing yet

---
+++ AddPin.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace HotPins {
+    internal class AddPin {
+        public static void Run(Terminal.ConsoleEventArgs args) {
+            Debug.Log("Hello, World!");
+        }
+    }
+}
--- HotPins.cs
+++ HotPins.cs
@@ -1,24 +1,34 @@
 ﻿using BepInEx;
 using HarmonyLib;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace HotPins {
-    [BepInPlugin("Flame.HotPins", "HotPins", "1.0")]
+    [BepInPlugin("Flame.HotPins", "HotPins", "1.0.0")]
     [BepInProcess("valheim.exe")]
     public class ExtendedMinimap : BaseUnityPlugin {
         private readonly Harmony harmony = new Harmony("Flame.HotPins");
+        public static Minimap minimapInstance;
 
         void Awake() {
             harmony.PatchAll();  //Patching the harmony
-
-
         }
 
-        /* Working with player markers */
-        [HarmonyPatch(typeof(Minimap), "UpdatePlayerMarker")]
-        class PlayerMarkerPatch {
-            private static void Postfix(ref RectTransform ___m_smallMarker, ref RectTransform ___m_smallShipMarker) {
+        /* Getting an instance of the minimap */
+        [HarmonyPatch(typeof(Minimap), "Awake")]
+        class GetMinimapInstance {
+            private static void Postfix(ref Minimap ___m_instance) {
+                minimapInstance = ___m_instance;
+            }
+        }
 
+        /* Assigning keys to control */
+        [HarmonyPatch(typeof(Terminal), nameof(Terminal.Awake))]
+        class AssignKeys {
+            private static void Postfix(ref Terminal __instance, ref List<Terminal.ConsoleCommand> ___m_commandList) {
+                /* Init add pin command */
+                Terminal.ConsoleEvent addPinEvent = AddPin.Run;  //Event that will occur after entering the command
+                Terminal.ConsoleCommand addPinCommand = new Terminal.ConsoleCommand("addpin", "create a pin", addPinEvent);
             }
         }
     }
