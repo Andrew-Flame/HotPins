ebae0cf7ac6a06c625727e74f8816d32e341e4f9
Andrew-Flame <andrewflame@mail.ru>
2023-01-17 16:58:19 +0300

    Add the ability to put pins on the map using the game terminal

--- AddPin.cs
+++ AddPin.cs
@@ -1,9 +1,24 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace HotPins {
     internal class AddPin {
+        /** Method for adding a pin to the game map */
         public static void Run(Terminal.ConsoleEventArgs args) {
-            Debug.Log("Hello, World!");
+            if (args.Length != 5) {  //If we don't have enough arguments
+                args.Context.AddString("Error!\nFour arguments are required:\npositionX positionY pinType pinName");
+                return;
+            }
+            
+            try {  //Trying to add a pin code to the card
+                Vector3 position = new Vector3(Convert.ToSingle(args.Args[1]), Convert.ToSingle(args.Args[2]), 0f);  //Get position as Vector3
+                Minimap.PinType pinType = (Minimap.PinType)Enum.Parse(typeof(Minimap.PinType), args.Args[3]);  //Get pin type
+                string name = args.Args[4];  //Get pin name
+                Main.minimapInstance.AddPin(position, pinType, name, true, false);  //Add pin to the map
+                Debug.Log($"The pin \"${name}\" was successfully marked on the map");
+            } catch {  //Otherwise, we output an error message
+                args.Context.AddString("Error!\nTry to check if the arguments are entered in the correct format and try again");
+            }
         }
     }
 }
