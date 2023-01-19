d4b6889b303f2c97dd2155b63e6fb5f42bb3484c
Andrew-Flame <andrewflame@mail.ru>
2023-01-19 13:54:38 +0300

    Add a more convenient dictionary for the type of pins

--- AddPin.cs
+++ AddPin.cs
@@ -1,8 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace HotPins {
     class AddPin {
+        private static readonly Dictionary<string, int> pinTypeDictionary = new Dictionary<string, int>() {
+            { "Fireplace", 0 },
+            { "House", 1 },
+            { "Hammer", 2 },
+            { "Ball", 3 },
+            { "Cave", 6 }
+        };  //Dictionary for default pin types
+
         /** Method for adding a pin to the game map */
         public static void Run(Terminal.ConsoleEventArgs args) {
             if (args.Length != 3) {  //If we don't have enough arguments
@@ -12,30 +21,14 @@ namespace HotPins {
             
             try {  //Trying to add a pin code to the card
                 Vector3 position = GamePlayer.GetPosition();  //Get the player's position as a pin position
-                Minimap.PinType pinType = (Minimap.PinType)Enum.Parse(typeof(Minimap.PinType), args.Args[1]);  //Get pin type
+                string pinTypeRaw = args.Args[1];  //Get the pin type as a raw string
+                Minimap.PinType pinType = (Minimap.PinType)pinTypeDictionary[pinTypeRaw];  //Get the pin type as a standard value
                 string name = args.Args[2];  //Get pin name
                 GameMinimap.GetInstance().AddPin(position, pinType, name, true, false);  //Add pin to the map
-                Debug.Log($"The pin \"{name}\" (type: {pinType}) was successfully marked on the map {{{Math.Round(position.x)}:{Math.Round(position.z)}}}");
+                Debug.Log($"The pin \"{name}\" (type: {pinTypeRaw}) was successfully marked on the map {{{Math.Round(position.x)}:{Math.Round(position.z)}}}");
             } catch {  //Otherwise, we output an error message
                 args.Context.AddString("Error!\nTry to check if the arguments are entered in the correct format and try again");
             }
-
-            /*
-             * Icon0,
-             * Icon1,
-             * Icon2,
-             * Icon3,
-             * Death,
-             * Bed,
-             * Icon4,
-             * Shout,
-             * None,
-             * Boss,
-             * Player,
-             * RandomEvent,
-             * Ping,
-             * EventArea
-            */
         }
     }
 }
