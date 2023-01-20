0b76c8e6c6968c9b13b9e602e66e9251819041bd
Andrew-Flame <andrewflame@mail.ru>
2023-01-20 23:00:57 +0300

    Add support for multi-key keyboard shortcuts

--- Master.cs
+++ Master.cs
@@ -2,15 +2,15 @@
 using HarmonyLib;
 using System.Collections.Generic;
 using System.IO;
+using System.Net.Configuration;
 using System.Text.RegularExpressions;
 using UnityEngine;
 
 namespace HotPins {
     [BepInPlugin("Flame.HotPins", "HotPins", "1.0.0")]
-    [BepInProcess("valheim.exe")]
     public class Master : BaseUnityPlugin {
         private readonly Harmony harmony = new Harmony("Flame.HotPins");
-        private Dictionary<KeyCode, Pin> keyBundles = new Dictionary<KeyCode, Pin>();  //A bundle of keys and pins that will be marked on the map using these keys
+        private Dictionary<KeyCode[], Pin> keyBundles = new Dictionary<KeyCode[], Pin>();  //A bundle of keys and pins that will be marked on the map using these keys
 
         void Awake() {
             harmony.PatchAll();  //Patching the harmony
@@ -20,21 +20,37 @@ namespace HotPins {
             foreach (string line in config.ReadToEnd().Split('\n')) {
                 if (Regex.IsMatch(line.Trim(), @"^\S+\s*=\s*""(Fireplace|House|Hammer|Ball|Cave)""\s*"".*""$")) {  //Check the string is a key-pin combination
                     string[] keyValueBandle = line.Trim().Split('=');  //Get key-value bundle
-                    KeyCode keyCode = (KeyCode)System.Enum.Parse(typeof(KeyCode), keyValueBandle[0].Trim());  //Get keycode
 
+                    /* Get keycodes shortcut */
+                    string[] keyCodesAsStrings = keyValueBandle[0].Trim().Split('+');  //Get array of keycodes for keyboard shortcut as strings
+                    KeyCode[] keyCodes = new KeyCode[keyCodesAsStrings.Length];  //Init an array for keaycodes
+                    for (byte i = 0; i < keyCodes.Length; i++) keyCodes[i] = (KeyCode)System.Enum.Parse(typeof(KeyCode), keyCodesAsStrings[i]);  //Set all keycodes
+
+                    /* Get pin info */
                     string[] typeNameBundle = Regex.Replace(keyValueBandle[1].Trim(), @"""\s*""", "\"\"").Split(new char[] { '\"' }, 
                         System.StringSplitOptions.RemoveEmptyEntries);  //Get pinType-pinName bundle
                     Pin pin = new Pin(typeNameBundle[0].Trim(), typeNameBundle[1].Trim());  //Create a pin instance
 
-                    keyBundles.Add(keyCode, pin);  //Add a key-pin bundle to dictionary
+                    keyBundles.Add(keyCodes, pin);  //Add a key-pin bundle to dictionary
                 }
             }
             config.Close();  //Close the stream
         }
 
         void Update() {
-            foreach (KeyValuePair<KeyCode, Pin> bundle in keyBundles)  //Checking whether any of the custom keys are pressed
-                if (Input.GetKeyDown(bundle.Key)) AddPin.Run(bundle.Value);  //If pressed, add the pin to the game map*/
+            foreach (KeyValuePair<KeyCode[], Pin> bundle in keyBundles) {  //Checking whether any of the custom keys are pressed
+                if (CheckKeys(bundle.Key)) {
+                    AddPin.Run(bundle.Value);  //If all the necessary keys are pressed, add the pin to the map
+                    return;  //You cannot add multiple pins in one frame, so we exit the method
+                }
+            }
+
+            /* Using this method, we check whether all the necessary keys are pressed */
+            bool CheckKeys(KeyCode[] keyCodes) {
+                if (!Input.GetKeyDown(keyCodes[keyCodes.Length - 1])) return false;  //Check the last pressed key (if it is not pressed, then the code should not be executed)
+                for (byte i = 0; i < keyCodes.Length - 1; i++) if (!Input.GetKey(keyCodes[i])) return false;  //If at least one key is not pressed, exit the method
+                return true;  //If the code execution has reached here, all the necessary keys are pressed
+            }
         }
     }
 }
