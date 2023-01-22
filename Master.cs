827541ae410fa3ae48d03daf8d721bc3a9a6f242
Andrew-Flame <andrewflame@mail.ru>
2023-01-22 12:40:18 +0300

    Refactor the code

--- Master.cs
+++ Master.cs
@@ -27,25 +27,25 @@ namespace HotPins {
             }
 
             /* Reading config file */
-            StreamReader config = new StreamReader(configPath);
-            foreach (string line in config.ReadToEnd().Split('\n')) {
-                if (Regex.IsMatch(line.Trim(), @"^\S+\s*=\s*""(Fireplace|House|Hammer|Ball|Cave)""\s*"".*""$")) {  //Check the string is a key-pin combination using regex
-                    string[] keyValueBandle = line.Trim().Split('=');  //Get key-value bundle
+            using (StreamReader config = new StreamReader(configPath)) {
+                foreach (string line in config.ReadToEnd().Split('\n')) {
+                    if (Regex.IsMatch(line.Trim(), @"^\S+\s*=\s*""(Fireplace|House|Hammer|Ball|Cave)""\s*"".*""$")) {  //Check the string is a key-pin combination using regex
+                        string[] keyValueBandle = line.Trim().Split('=');  //Get key-value bundle
 
-                    /* Get keycodes shortcut */
-                    string[] keyCodesAsStrings = keyValueBandle[0].Trim().Split('+');  //Get array of keycodes for keyboard shortcut as strings
-                    KeyCode[] keyCodes = new KeyCode[keyCodesAsStrings.Length];  //Init an array for keaycodes
-                    for (byte i = 0; i < keyCodes.Length; i++) keyCodes[i] = (KeyCode)System.Enum.Parse(typeof(KeyCode), keyCodesAsStrings[i]);  //Set all keycodes
+                        /* Get keycodes shortcut */
+                        string[] keyCodesAsStrings = keyValueBandle[0].Trim().Split('+');  //Get array of keycodes for keyboard shortcut as strings
+                        KeyCode[] keyCodes = new KeyCode[keyCodesAsStrings.Length];  //Init an array for keaycodes
+                        for (byte i = 0; i < keyCodes.Length; i++) keyCodes[i] = (KeyCode)System.Enum.Parse(typeof(KeyCode), keyCodesAsStrings[i]);  //Set all keycodes
 
-                    /* Get pin info */
-                    string[] typeNameBundle = Regex.Replace(keyValueBandle[1].Trim(), @"""\s*""", "\"\"").Split(new char[] { '\"' }, 
-                        System.StringSplitOptions.RemoveEmptyEntries);  //Get pinType-pinName bundle
-                    Pin pin = new Pin(typeNameBundle[0].Trim(), typeNameBundle[1].Trim());  //Create a pin instance
+                        /* Get pin info */
+                        string[] typeNameBundle = Regex.Replace(keyValueBandle[1].Trim(), @"""\s*""", "\"\"").Split(new char[] { '\"' },
+                            System.StringSplitOptions.RemoveEmptyEntries);  //Get pinType-pinName bundle
+                        Pin pin = new Pin(typeNameBundle[0].Trim(), typeNameBundle[1].Trim());  //Create a pin instance
 
-                    keyBundles.Add(keyCodes, pin);  //Add a key-pin bundle to dictionary
+                        keyBundles.Add(keyCodes, pin);  //Add a key-pin bundle to dictionary
+                    }
                 }
             }
-            config.Close();  //Close the stream
 
             /* Sorting binds (we need the first keyboard shortcuts in the dictionary with the largest number of keys involved) */
             {
