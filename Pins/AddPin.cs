8e4aaf76c4902e0fb5eee983df34e680c4b24823
Andrew-Flame <andrewflame@mail.ru>
2023-02-03 01:12:59 +0300

    Delete the terminal command addpin

--- Pins/AddPin.cs
+++ Pins/AddPin.cs
@@ -12,26 +12,11 @@ namespace HotPins {
             { "Cave", 6 }
         };  //Dictionary for default pin types
 
-        /** Method for adding a pin to the game map */
-        public static void Run(Terminal.ConsoleEventArgs args) {
-            if (args.Length != 3) {  //If we don't have enough arguments
-                args.Context.AddString("Error!\nTwo arguments are required\nArguments: pin-type pin-name");
-                return;
-            }
-            
-            try {  //Trying to add a pin to the map
-                string pinTypeRaw = args.Args[1];  //Get the pin type as a raw string
-                string pinName = args.Args[2];  //Get the pin name
-                SetPin(pinTypeRaw, pinName);  //Set the pin
-            } catch {  //Otherwise, we output an error message
-                args.Context.AddString("Error!\nTry to check if the arguments are entered in the correct format and try again");
-            }
-        }
         public static void Run(Pin pin) {
             SetPin(pin.getType(), pin.getName());
         }
 
-        private static void SetPin(String pinTypeRaw, string pinName) {
+        private static void SetPin(string pinTypeRaw, string pinName) {
             if (GameMinimap.GetInstance() == null) return;  //If there is no instance of the minimap, then the player is in the main menu
             Vector3 position = GamePlayer.GetPosition();  //Get the player's position as a pin position
             Minimap.PinType pinType = (Minimap.PinType)pinTypeDictionary[pinTypeRaw];  //Get the pin type as a standard value
