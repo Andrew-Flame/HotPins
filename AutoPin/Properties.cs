53fb38115eddf9e711689fe00eae156670b288a4
Andrew-Flame <andrewflame@mail.ru>
2023-03-09 15:15:32 +0300

    Refactor and optimize the code

---
+++ AutoPin/Properties.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace HotPins.AutoPin; 
+
+internal struct Properties {
+    public KeyCode Key { get; set; }
+    public int Radius { get; set; }
+    public int SqrtRadius => Radius * Radius;
+    public string Type { get; set; }
+    public string[] Names { get; }
+
+    public Properties(params string[] names) => Names = names;
+}
