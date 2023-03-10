3f7426889d0861cca59c55e020c9782f60e00396
Andrew-Flame <andrewflame@mail.ru>
2023-03-10 11:46:08 +0300

    Update the assembly version

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -4,6 +4,7 @@ using HotPins;
 
 [assembly: AssemblyTitle(ModInfo.MODNAME)]
 [assembly: AssemblyCompany(ModInfo.AUTHOR)]
+[assembly: AssemblyVersion(ModInfo.VERSION)]
 [assembly: AssemblyDescription("Modification for Valheim, which adds hotkeys to create pins")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 
