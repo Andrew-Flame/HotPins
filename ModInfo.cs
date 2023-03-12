61f1ec9e95e3ba638c5b5092b1700dbb263bfa70
Andrew-Flame <andrewflame@mail.ru>
2023-03-12 16:17:58 +0300

    Update the build version

--- ModInfo.cs
+++ ModInfo.cs
@@ -4,5 +4,5 @@ internal readonly struct ModInfo {
     public const string MODNAME = "HotPins",
         AUTHOR = "Flame",
         GUID = AUTHOR + "." + MODNAME,
-        VERSION = "1.6.1";
+        VERSION = "1.6.2";
 }
