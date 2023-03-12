af825c36048db13f1d91cc5eec707cbcd440e545
Andrew-Flame <andrewflame@mail.ru>
2023-03-12 16:17:25 +0300

    Fixed an online game bug

--- GameClasses/GamePlayer.cs
+++ GameClasses/GamePlayer.cs
@@ -6,11 +6,15 @@ internal static class GamePlayer {
     /// <summary>An instance of the game player</summary>
     private static Player _instance;
 
-    /// <summary>Get an instance of the game character's visual</summary>
-    [HarmonyPatch(typeof(Player), "Awake")]
-    private static class GetCharacterVisualInstance {
+    /// <summary>Get an instance of the game character</summary>
+    [HarmonyPatch(typeof(Player), "SetPlayerID")]
+    private static class GetCharacterInstance {
         private static void Postfix(ref Player __instance) {
-            _instance = __instance; 
+            long id = __instance.GetPlayerID();  //Get player id
+            string name = __instance.GetPlayerName();  //Get player name
+            /* If this player is our, update the current player's field */
+            if (id == GamePlayerProfile.GetPlayerId() && name == GamePlayerProfile.GetPlayerName()) 
+                _instance = __instance;
         }
     }
 
